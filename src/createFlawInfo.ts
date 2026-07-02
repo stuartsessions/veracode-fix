7e58aff735ccea9c2db0cbe9122756c80673c5f9
Stuart Sessions <stsessions@veracode.com>
2026-07-02 11:39:19 -0700

    added logging

--- src/createFlawInfo.ts
+++ src/createFlawInfo.ts
@@ -33,7 +33,9 @@ export async function createFlawInfo(flawInfo:any,options:any){
     let flows:any = []
 
     // Safely access Frame using optional chaining
-    resultArray.stack_dumps?.stack_dump?.[0]?.Frame?.forEach((element: any) => {
+    // If no frame exists, flows remains empty.
+    console.log(resultArray.stack_dumps?.stack_dump?.[0]?.Frame)
+    await resultArray.stack_dumps?.stack_dump?.[0]?.Frame?.forEach((element: any) => {
         if (element.SourceFile == sourceFile && element.VarNames != undefined){
             if (options.DEBUG == 'true'){
                 console.log('#######- DEBUG MODE -#######')
